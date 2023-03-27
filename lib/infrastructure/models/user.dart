51b7239f48d50124bf4e9716f96a8b44629a0f53
Carlos Maldonado <carlosmaldonado@MacBook-Pro-de-Carlos.local>
2023-03-27 08:34:18 -0700

    nada

---
+++ lib/infrastructure/models/user.dart
@@ -0,0 +1,39 @@
+enum Genre { male, female, other }
+
+enum Intereses {
+  tiendasDeRopa,
+  esteticas,
+  barberias,
+  supermercados,
+  restaurantes,
+}
+
+class User {
+  final String uid;
+  final String name;
+  final String middleName;
+  final String lastName;
+  final int age;
+  final Genre genre;
+  final String email;
+  final String password;
+  final String phoneNumber;
+  final List<Intereses>? intereses;
+  //final Location location;
+
+  User({
+    required this.uid,
+    required this.name,
+    required this.middleName,
+    required this.lastName,
+    required this.age,
+    required this.genre,
+    required this.email,
+    required this.password,
+    required this.phoneNumber,
+    this.intereses,
+    //required this.location,
+  });
+}
+
+//coleccition users..
