db6bb063c515758e263fdac222995b097031ce7e
CARLOS MALDONADO <karloz_0986@hotmail.com>
2023-04-10 16:51:01 -0700

    nmada

--- lib/presentation/screens/aviso_privacidad/aviso_privacidad_screen.dart
+++ lib/presentation/screens/aviso_privacidad/aviso_privacidad_screen.dart
@@ -7,9 +7,20 @@ class AvisoPrivacidadScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(title: 'Aviso de privacidad'),
-      body: Placeholder(),
+    return Scaffold(
+      appBar: const CustomAppBar(title: 'Aviso de privacidad'),
+      body: Container(
+        color: Colors.white,
+        child: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Center(
+            child: Text(
+                style: TextStyle(fontSize: 14),
+                textAlign: TextAlign.justify,
+                'Shopping MX, con domicilio en Av. Narciso Mendoza 9, Col. Manuel Ávila Camacho. Naucalpan, Edo. de México, es responsable de recabar sus  datos personales, del uso que se le dé a los mismos y de su protección en términos de lo dispuesto en la Ley Federal de Protección de Datos Personales en Posesión de Particulares, su Reglamento y demás disposiciones aplicables (en adelante la “Ley de Datos”), por lo que pone a su disposición el presente Aviso de Privacidad.'),
+          ),
+        ),
+      ),
     );
   }
 }
