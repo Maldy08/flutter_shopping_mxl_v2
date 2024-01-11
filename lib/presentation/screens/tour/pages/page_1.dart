5f6c7e29815344e6911cf42c951bc96de9b99d3e
Carlos Maldonado <32904610+Maldy08@users.noreply.github.com>
2024-01-11 07:56:57 -0800

    nada

--- lib/presentation/screens/tour/pages/page_1.dart
+++ lib/presentation/screens/tour/pages/page_1.dart
@@ -1,60 +1,44 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class Page1 extends StatelessWidget {
   const Page1({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Page 1'),
+    return Container(
+      padding: const EdgeInsets.only(top: 50),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/onboarding-1.png',
+            fit: BoxFit.contain,
+            width: 350,
+            height: 350,
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            'Bienvenido a EnOfferta',
+            style: TextStyle(
+                fontSize: 24,
+                color: Colors.black,
+                fontFamily: 'Roboto',
+                decoration: TextDecoration.none,
+                fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            'Donde podrás encontrar cupones y descuentos de comercios de la localidad.',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 16,
+                color: Colors.black45,
+                fontFamily: 'Roboto',
+                decoration: TextDecoration.none,
+                fontWeight: FontWeight.normal),
+          ),
+        ],
+      ),
     );
   }
 }
-
-
-//  Column(
-//               crossAxisAlignment: CrossAxisAlignment.center,
-//               children: [
-//                 Image.asset(
-//                   'assets/images/onboarding-1.png',
-//                   fit: BoxFit.contain,
-//                   width: 350,
-//                   height: 350,
-//                 ),
-//                 const SizedBox(height: 20),
-//                 const Text(
-//                   'Bienvenido a EnOfferta',
-//                   style: TextStyle(
-//                       fontSize: 24,
-//                       color: Colors.black,
-//                       fontFamily: 'Roboto',
-//                       decoration: TextDecoration.none,
-//                       fontWeight: FontWeight.w600),
-//                 ),
-//                 const SizedBox(height: 20),
-//                 const Text(
-//                   'Donde podrás encontrar cupones y descuentos de comercios de la localidad.',
-//                   textAlign: TextAlign.center,
-//                   style: TextStyle(
-//                       fontSize: 16,
-//                       color: Colors.black45,
-//                       fontFamily: 'Roboto',
-//                       decoration: TextDecoration.none,
-//                       fontWeight: FontWeight.normal),
-//                 ),
-//                 const SizedBox(height: 20),
-//                 const Spacer(),
-//                 Padding(
-//                   padding: const EdgeInsets.all(8.0),
-//                   child: Row(
-//                     children: [
-//                       const Spacer(),
-//                       CustomElevatedButton(
-//                         label: 'Siguiente',
-//                         func: () {},
-//                       )
-//                     ],
-//                   ),
-//                 )
-//               ],
-//             ),
--- lib/presentation/screens/tour/tour_screen.dart
+++ lib/presentation/screens/tour/tour_screen.dart
@@ -15,10 +15,7 @@ class _TourScreenState extends State<TourScreen> {
   @override
   void initState() {
     super.initState();
-    _pageController = PageController(
-      keepPage: true,
-      viewportFraction: 1,
-    );
+    _pageController = PageController();
   }
 
   @override
@@ -27,43 +24,109 @@ class _TourScreenState extends State<TourScreen> {
     _pageController.dispose();
   }
 
-  void onNextPage() {
-    if (_activePage < viewRoutes.length - 1) {
-      _pageController.nextPage(
-        duration: const Duration(milliseconds: 500),
-        curve: Curves.fastEaseInToSlowEaseOut,
-      );
-    }
+  static const _kDuration = Duration(milliseconds: 300);
+  static const _kCurve = Curves.ease;
+
+  nextFunction() {
+    _pageController.nextPage(duration: _kDuration, curve: _kCurve);
   }
 
-  final viewRoutes = const <Widget>[
-    Page1(),
-    Page2(),
-    Page3(),
-  ];
+  previousFunction() {
+    _pageController.previousPage(duration: _kDuration, curve: _kCurve);
+  }
+
+  onChangedFunction(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
 
   late PageController _pageController = PageController();
-
-  int _activePage = 0;
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        color: Colors.white,
-        child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: PageView(
-              physics: const NeverScrollableScrollPhysics(),
-              controller: _pageController,
-              children: viewRoutes,
-              onPageChanged: (value) {
-                setState(() {
-                  _activePage = value;
-                });
-              },
-            )),
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            controller: _pageController,
+            children: const [
+              Page1(),
+              Page2(),
+              Page3(),
+            ],
+          ),
+          Positioned(
+            bottom: 80,
+            left: 200,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Indicator(
+                  positionIndex: 0,
+                  currentIndex: currentIndex,
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Indicator(
+                  positionIndex: 1,
+                  currentIndex: currentIndex,
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Indicator(
+                  positionIndex: 2,
+                  currentIndex: currentIndex,
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 30,
+            left: 130,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Container(
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    InkWell(
+                        onTap: () => previousFunction(),
+                        child: Text("Previous")),
+                    const SizedBox(
+                      width: 50,
+                    ),
+                    InkWell(onTap: () => nextFunction(), child: Text("Next"))
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class Indicator extends StatelessWidget {
+  final int positionIndex, currentIndex;
+  const Indicator({super.key, this.currentIndex = 1, this.positionIndex = 1});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 12,
+      width: 12,
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.blue),
+          color:
+              positionIndex == currentIndex ? Colors.blue : Colors.transparent,
+          borderRadius: BorderRadius.circular(100)),
     );
   }
 }
