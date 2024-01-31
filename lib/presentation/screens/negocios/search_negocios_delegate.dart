7b6a7833a20a658542aebb33d7e7af9800db847c
Carlos Maldonado <32904610+Maldy08@users.noreply.github.com>
2024-01-31 07:53:39 -0800

    nada

--- lib/presentation/screens/negocios/search_negocios_delegate.dart
+++ lib/presentation/screens/negocios/search_negocios_delegate.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_shopping_mxl_v2/infrastructure/models/models.dart';
 
@@ -16,6 +17,11 @@ class SearchNegociosDelegate extends SearchDelegate {
           icon: const Icon(Icons.clear))
     ];
   }
+
+  @override
+  String? get searchFieldLabel => 'Buscar afiliado';
+  @override
+  TextStyle? get searchFieldStyle => const TextStyle(fontSize: 16);
 
   @override
   Widget? buildLeading(BuildContext context) {
@@ -55,14 +61,72 @@ class SearchNegociosDelegate extends SearchDelegate {
     return ListView.builder(
       itemCount: suggestionList.length,
       itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(suggestionList[index].nombreEmpresa),
-          onTap: () {
-            query = suggestionList[index].id;
-            // Show the search results based on the selected suggestion.
+        return _NegocioItem(
+          negocio: suggestionList[index],
+          onNegocioSelected: (context, negocio) {
+            // close(context, negocio);
+            //context.push('/home/0/negocio/${negocio.id}');
           },
         );
       },
     );
   }
 }
+
+class _NegocioItem extends StatelessWidget {
+  final Negocios negocio;
+  final Function onNegocioSelected;
+
+  const _NegocioItem({required this.negocio, required this.onNegocioSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return GestureDetector(
+      onTap: () {
+        onNegocioSelected(context, negocio);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 5,
+        ),
+        child: Row(
+          children: [
+            SizedBox(
+              width: size.width * 0.2,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  negocio.photoUrl,
+                  loadingBuilder: (context, child, loadingProgress) =>
+                      FadeIn(child: child),
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+            SizedBox(
+              width: size.width * 0.7,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(negocio.nombreEmpresa),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+// ListTile(
+//           title: Text(suggestionList[index].nombreEmpresa),
+//           onTap: () {
+//             query = suggestionList[index].id;
+//             // Show the search results based on the selected suggestion.
+//           },
+//         );
